be55322e9a4a860fc2a536b92a8364fe69e1d1f4
zannatul <jannatulfahmida15@gmail.com>
2022-12-01 19:18:45 +0600

    first commit

---
+++ src/components/ProductCard.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { BiListPlus, BiTrash } from "react-icons/bi";
+import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { addToCart } from "../redux/actionCreators/productActions";
+import { ADD_TO_CART } from "../redux/actionTypes/actionTypes";
+
+const ProductCard = ({ product }) => {
+  const location = useLocation();
+  const dispatch = useDispatch();
+
+  return (
+    <div
+      className="shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900"
+      key={product._id}
+    >
+      <div className="h-52 w-52 mx-auto">
+        <img src={product.image} alt={product.model} />
+      </div>
+      <h1 className="font-bold text-center">{product.model}</h1>
+      <p className="text-center font-semibold mb-3">Rating: {product.rating}</p>
+      <div className=" flex-1">
+        <ul className="space-y-2">
+          {product.keyFeature.map((feature) => {
+            return (
+              <li key={feature} className="text-sm ">
+                {feature}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="flex gap-2 mt-5">
+        <button
+          onClick={() => dispatch(addToCart(product))}
+          className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
+        >
+          Add to cart
+        </button>
+        <button
+          title="Add to wishlist"
+          className="bg-indigo-500  py-1 px-2 rounded-full"
+        >
+          <BiListPlus className="text-white" />
+        </button>
+        {location.pathname === "/cart" && (
+          <button
+            title="Remove from cart"
+            className="bg-indigo-500  py-1 px-2 rounded-full"
+          >
+            <BiTrash className="text-white" />
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
---
+++ src/pages/WishList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WishList = () => {
+    
+      return (
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
+          this is wishlist
+        </div>
+      );
+};
+
+export default WishList;
